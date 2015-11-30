1363c54be71cfa67e6da09e4b8d7b346dbeea789
tvanderbruggen <th.vanderbruggen@gmail.com>
2015-11-30 17:46:41 +0000

    Add doc

--- examples/coherent_velocimeter.py
+++ examples/coherent_velocimeter.py
@@ -12,11 +12,31 @@ import matplotlib.pyplot as plt
 import peakutils
 
 class CoherentVelocimeter:
+    """ Coherent velocitor based on the Spectrum bitstream
+    """
     
     def __init__(self, lambda_opt=1.55E-6):
+        """
+            Args:
+                - lambda_opt: Optical wavelength (m)
+        """        
         self.lambda_opt = lambda_opt
         
-    def get_simul_data(self, velocity, SNR=10, n_pts=8142, fs = 125e6, plot=False):
+    def get_data(self):
+        """ Load spectrum data from the Red Pitaya
+        """
+        pass
+        
+    def get_simul_data(self, velocity, SNR, n_pts=8142, fs=125e6, plot=False):
+        """ Load simulated spectrum data
+        
+            Args:
+                - velocity: Object velocity (m/s)
+                - SNR: Signal-to-noise
+                - n_pts: Number of samples
+                - fs: Sampling rate (S/s)
+                - plot: Set to true to plot the simulated data
+        """
         time = np.linspace(0, n_pts/fs, n_pts)
         omega_doppler = 2*np.pi*velocity/self.lambda_opt # Doppler shift (rad/s)
         detected_signal = SNR * np.cos(omega_doppler*time) + np.random.randn(n_pts)
@@ -33,6 +53,11 @@ class CoherentVelocimeter:
         self.PSD = PSD
     
     def _psd_filter(self, plot=False):
+        """ Filter the PSD
+        
+            Args:
+                - plot: Set to True to plot the result
+        """    
         window = signal.general_gaussian(51, p=0.5, sig=10)
         psd_filtered = signal.fftconvolve(window, self.PSD)
         psd_filtered = (np.average(self.PSD) / np.average(psd_filtered)) * psd_filtered
@@ -47,6 +72,11 @@ class CoherentVelocimeter:
         return psd_filtered
     
     def _peak_detection(self, plot=False):
+        """ Performs the peak detection on the PSD
+        
+             Args:
+                - plot: Set to True to plot the result       
+        """
         peakind = peakutils.indexes(self._psd_filter(), thres=0.5, min_dist=30)
         
         if plot:
@@ -57,9 +87,21 @@ class CoherentVelocimeter:
             plt.xlabel('Frequency [MHz]')
             plt.ylabel('PSD')
             plt.show()
+            
+        return self.f[peakind]
+            
+    def get_velocity(self):
+        """ Return the velocity in m/s
+        """
+        f_peak = self._peak_detection()
+        
+        # TODO Handle the case where several peaks are detected
+        
+        return self.lambda_opt * f_peak
         
 if __name__ == "__main__":
     lidar = CoherentVelocimeter()
     lidar.get_simul_data(5.2, SNR=5, plot=False)
 #    lidar._psd_filter(plot=True)
     lidar._peak_detection(plot=True)
+    print lidar.get_velocity()
