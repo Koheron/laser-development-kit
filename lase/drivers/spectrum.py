ffe613cb059630049af8fa89493c8dc913e17830
Jean Minet <jean.minet@gmail.com>
2015-12-06 02:16:38 +0100

    spectrum analyzer working

--- lase/drivers/spectrum.py
+++ lase/drivers/spectrum.py
@@ -20,13 +20,14 @@ class Spectrum(Lase):
         # \end
         
         # offsets
-        self._subtract_mean_re_off = 24
-        self._subtract_mean_im_off = 28
-        self._scale_sch_off = 32
+        self._subtract_mean_off = 24
+        self._cfg_fft_off = 28
+        self._demod_off = 32
+        self._avg_off_off = 36
 
         # Add memory maps
         self._spectrum = self.dvm.add_memory_map(_spectrum_addr, self.sampling.n/1024*map_size)
-        self._demod = self.dvm.add_memory_map(_demod_addr, self.sampling.n/1024*map_size)
+#        self._demod = self.dvm.add_memory_map(_demod_addr, self.sampling.n/1024*map_size)
 
         self.spectrum = np.zeros(self.sampling.n, dtype=np.float32)
         self.demod = np.zeros((2,self.sampling.n))
@@ -34,40 +35,38 @@ class Spectrum(Lase):
         self.demod[0,:] = 0.49 * (1- np.cos(2*np.pi*np.arange(self.sampling.n)/self.sampling.n))   #0.5*np.real(demod) 
         self.demod[1,:] = 0#0.5*np.imag(demod)
 
-        self.set_offset_real(-199)
-        self.set_offset_real(-21)
+        self.set_offset(-199, -21)
+        #self.set_offset(0, 0)
 
-        self.set_demod()
-        self.dvm.write(self._config, self._scale_sch_off, 427)
+#        self.set_demod()
+#        self.dvm.write(self._config, self._scale_sch_off, 427)
         
         self.reset()
     
     def set_scale_sch(self, scale_sch):
-        self.dvm.write(self._config,self._scale_sch_off, scale_sch)
+        self.dvm.write(self._config,self._cfg_fft_off, 1 + 2 * scale_sch)
            
-    def set_offset(self, offset):
-        self.dvm.write(self._config,self._subtract_mean_re_off, offset)
-        self.dvm.write(self._config,self._subtract_mean_im_off, offset)
+    def set_offset(self, offset_real, offset_imag):
+        self.dvm.write(self._config,self._subtract_mean_off, offset_real + 2**14 * offset_imag)
 
-    def set_offset_real(self, offset):
-        self.dvm.write(self._config,self._subtract_mean_re_off, offset)
+    def set_demod(self):
+        self.dvm.write(self._config,self._demod_off, 8192 + 65356 * 8192)
 
-    def set_offset_imag(self, offset):
-        self.dvm.write(self._config,self._subtract_mean_im_off, offset)
-
-    def set_demod(self, warning=False):
-        if warning:
-            if np.max(np.abs(self.demod)) >= 1:
-                print 'WARNING : dac out of bounds'                
-        demod_data_1 = np.mod(np.floor(8192*self.demod[0,:]) + 8192,16384)+8192
-        demod_data_2 = np.mod(np.floor(8192*self.demod[1,:]) + 8192,16384)+8192
-        self.dvm.write_buffer(self._demod, 0, demod_data_1 + 65536 * demod_data_2)
+#    def set_demod(self, warning=False):
+#        if warning:
+#            if np.max(np.abs(self.demod)) >= 1:
+#                print 'WARNING : dac out of bounds'
+#        demod_data_1 = np.mod(np.floor(8192*self.demod[0,:]) + 8192,16384)+8192
+#        demod_data_2 = np.mod(np.floor(8192*self.demod[1,:]) + 8192,16384)+8192
+#        self.dvm.write_buffer(self._demod, 0, demod_data_1 + 65536 * demod_data_2)
         
     def get_spectrum(self):
         self.dvm.set_bit(self._config, self._addr_off,1)
         time.sleep(0.001)
         self.spectrum = self.dvm.read_buffer(self._spectrum, 0, self.sampling.n, data_type='float32')
         n_avg = self.dvm.read(self._status,self._n_avg1_off)
+        print n_avg
         self.spectrum = self.spectrum / np.float(n_avg)
+        self.spectrum[1] = 1
         self.dvm.clear_bit(self._config, self._addr_off,1)
         
