0a06e5541b2140fb3442f2fbd779525cfe6d168a
Jean Minet <jean.minet@gmail.com>
2015-12-12 20:31:46 +0100

    clean stats widget

--- lase/gui/stats_widget.py
+++ lase/gui/stats_widget.py
@@ -12,69 +12,52 @@ class StatsWidget(QtGui.QWidget):
 
         self.layout = QtGui.QHBoxLayout()
 
-        # Measures
-        ## Name
-        self.mean_label = QtGui.QLabel('Average')
-        self.amp_label = QtGui.QLabel('Amplitude')
-        self.mean_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.amp_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.mean_labels = []
+        self.ampl_labels = []
+        self.n_channels = 2
 
-        ## adc_1
-        self.mean_1_label = QtGui.QLabel('')
-        self.amp_1_label= QtGui.QLabel('')
-        self.mean_1_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.amp_1_label.setAlignment(QtCore.Qt.AlignCenter)
-        ## adc_2
-        self.mean_2_label = QtGui.QLabel('')
-        self.amp_2_label = QtGui.QLabel('')
-        self.mean_2_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.amp_2_label.setAlignment(QtCore.Qt.AlignCenter)
+        for i in range(self.n_channels+1):
+            self.mean_labels.append(QtGui.QLabel(''))
+            self.ampl_labels.append(QtGui.QLabel(''))
+            self.mean_labels[i].setAlignment(QtCore.Qt.AlignCenter)
+            self.ampl_labels[i].setAlignment(QtCore.Qt.AlignCenter)
 
-        self.measures_box = QtGui.QGroupBox('Measures')
-        self.measures_box.setAlignment(5)
-        self.adc_1_box = QtGui.QGroupBox('ADC 1')
-        self.adc_1_box.setAlignment(5)
-        self.adc_2_box = QtGui.QGroupBox('ADC 2')
-        self.adc_2_box.setAlignment(5)
+        self.mean_labels[0].setText('Average')
+        self.ampl_labels[0].setText('Amplitude')
+
+        self.boxes = []
+        self.box_names = ['Measures', 'ADC 1', 'ADC 2']
+        for i in range(self.n_channels+1):
+            self.boxes.append(QtGui.QGroupBox(self.box_names[i]))
+            self.boxes[i].setAlignment(5)
 
         self.name_layout = QtGui.QVBoxLayout()
-
-        self.name_layout.addWidget(self.mean_label)
-        self.name_layout.addWidget(self.amp_label)
-        self.measures_box.setLayout(self.name_layout)
+        self.name_layout.addWidget(self.mean_labels[0])
+        self.name_layout.addWidget(self.ampl_labels[0])
+        self.boxes[0].setLayout(self.name_layout)
 
         self.adc_1_layout = QtGui.QVBoxLayout()
+        self.adc_1_layout.addWidget(self.mean_labels[1])
+        self.adc_1_layout.addWidget(self.ampl_labels[1])
+        self.boxes[1].setLayout(self.adc_1_layout)
+
         self.adc_2_layout = QtGui.QVBoxLayout()
+        self.adc_2_layout.addWidget(self.mean_labels[2])
+        self.adc_2_layout.addWidget(self.ampl_labels[2])
+        self.boxes[2].setLayout(self.adc_2_layout)
 
-        self.adc_1_layout.addWidget(self.mean_1_label)
-        self.adc_1_layout.addWidget(self.amp_1_label)
-        self.adc_1_box.setLayout(self.adc_1_layout)
-
-        self.adc_2_layout.addWidget(self.mean_2_label)
-        self.adc_2_layout.addWidget(self.amp_2_label)
-        self.adc_2_box.setLayout(self.adc_2_layout)
-
-        self.layout.addWidget(self.measures_box)
-        self.layout.addWidget(self.adc_1_box)
-        self.layout.addWidget(self.adc_2_box)
+        for i in range(self.n_channels+1):
+            self.layout.addWidget(self.boxes[i])
 
     def update(self):
-        if 1e-2 < np.abs(np.mean(self.driver.adc[0,:])) < 1e3:
-            self.mean_1_label.setText('{:.2f}'.format(np.mean(self.driver.adc[0,:])))
-        else:
-            self.mean_1_label.setText('%.2e'%(np.mean(self.driver.adc[0,:])))
-
-        if 1e-2 < np.abs(np.mean(self.driver.adc[1,:])) < 1e3:
-            self.mean_2_label.setText('{:.2f}'.format(np.mean(self.driver.adc[1,:])))
-        else:
-            self.mean_2_label.setText('%.2e'%(np.mean(self.driver.adc[1,:])))
-
-        if 1e-2 < np.abs(np.max(self.driver.adc[0,:])-np.min(self.driver.adc[0,:])) < 1e3:
-            self.amp_1_label.setText('{:.2f}'.format(np.max(self.driver.adc[0,:])-np.min(self.driver.adc[0,:])))
-        else:
-            self.amp_1_label.setText('%.2e'%(np.max(self.driver.adc[0,:])-np.min(self.driver.adc[0,:])))
-
-        if 1e-2 < np.abs(np.max(self.driver.adc[1,:])-np.min(self.driver.adc[1,:])) < 1e3:
-            self.amp_2_label.setText('{:.2f}'.format(np.max(self.driver.adc[1,:])-np.min(self.driver.adc[1,:])))
-        else:
-            self.amp_2_label.setText('%.2e'%(np.max(self.driver.adc[1,:])-np.min(self.driver.adc[1,:])))
+        for i in range(self.n_channels):
+            if 1e-2 < np.abs(np.mean(self.driver.adc[i,:])) < 1e3:
+                mean_text = '{:.2f}'.format(np.mean(self.driver.adc[i,:]))
+            else:
+                mean_text = '%.2e'%(np.mean(self.driver.adc[i,:]))
+            self.mean_labels[i+1].setText(mean_text)
+            if 1e-2 < np.abs(np.max(self.driver.adc[i,:])-np.min(self.driver.adc[i,:])) < 1e3:
+                ampl_text = '{:.2f}'.format(np.max(self.driver.adc[i,:])-np.min(self.driver.adc[i,:]))
+            else:
+                ampl_text = '%.2e'%(np.max(self.driver.adc[i,:])-np.min(self.driver.adc[i,:]))
+            self.ampl_labels[i+1].setText(ampl_text)
