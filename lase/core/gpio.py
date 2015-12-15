b47c2408ab3c69d56535bbea1d337e6315082373
minjung <minjung.kim@koheron.com>
2015-12-15 18:27:40 +0100

    clean gpio.py

--- lase/core/gpio.py
+++ lase/core/gpio.py
@@ -3,49 +3,48 @@ class Gpio():
     http://www.xilinx.com/support/documentation/ip_documentation/axi_gpio/v2_0/pg144-axi-gpio.pdf
     http://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit-in-c-c
     """
-    def __init__(self, dvm, addr = int('0x41200000',0), 
-                 map_size = 16 * 4096, n_channel_1 = 8, n_channel_2 = 8,
-                 dual_channel = True):
+    def __init__(self, dvm, addr=int('0x41200000', 0),
+                 map_size=16 * 4096, n_channel_1=8, n_channel_2=8,
+                 dual_channel=True):
         self.dvm = dvm
         self.dev_num = self.dvm.add_memory_map(addr, map_size)
-        
-    def set_bit(self, index, channel = 1):
+
+    def set_bit(self, index, channel=1):
         if channel == 2:
             offset = 8
         else:
             offset = 0
-            
+
         self.dvm.set_bit(self.dev_num, offset, index)
-        
-    def clear_bit(self, index, channel = 1):
+
+    def clear_bit(self, index, channel=1):
         if channel == 2:
             offset = 8
         else:
             offset = 0
-            
+
         self.dvm.clear_bit(self.dev_num, offset, index)
-        
-    def toggle_bit(self, index, channel = 1):
+
+    def toggle_bit(self, index, channel=1):
         if channel == 2:
             offset = 8
         else:
             offset = 0
-            
+
         self.dvm.toggle_bit(self.dev_num, offset, index)
-    
-    def set_as_input(self, index, channel = 1):
+
+    def set_as_input(self, index, channel=1):
         if channel == 2:
             offset = 12
         else:
             offset = 4
-            
+
         self.dvm.set_bit(self.dev_num, offset, index)
-        
-    def set_as_output(self, index, channel = 1):
+
+    def set_as_output(self, index, channel=1):
         if channel == 2:
             offset = 12
         else:
-            offset = 4 
-        
-        self.dvm.clear_bit(self.dev_num, offset, index)
+            offset = 4
 
+        self.dvm.clear_bit(self.dev_num, offset, index)
