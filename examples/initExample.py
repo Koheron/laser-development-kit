2f30fdb5e937da0345dcd739381e897900355931
minjung <minjung.kim@koheron.com>
2015-12-15 12:22:11 +0100

    update initExample.py

--- examples/initExample.py
+++ examples/initExample.py
@@ -1,7 +1,9 @@
-## make this version of lase importable before any others
-## we do this to make sure that, when running examples, the correct library
-## version is imported (if there are multiple versions present).
-import sys, os
+# make this version of lase importable before any others
+# we do this to make sure that, when running examples, the correct library
+# version is imported (if there are multiple versions present).
+
+import sys
+import os
 
 if not hasattr(sys, 'frozen'):
     if __file__ == '<stdin>':
@@ -10,11 +12,11 @@ if not hasattr(sys, 'frozen'):
         path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     path.rstrip(os.path.sep)
     if 'lase' in os.listdir(path):
-        sys.path.insert(0, path) ## examples adjacent to lase (as in source tree)
+        sys.path.insert(0, path)  # examples adjacent to lase (as in source tree)
     else:
         for p in sys.path:
             if len(p) < 3:
                 continue
-            if path.startswith(p):  ## If the example is already in an importable location, promote that location
+            if path.startswith(p):  # If the example is already in an importable location, promote that location
                 sys.path.remove(p)
                 sys.path.insert(0, p)
