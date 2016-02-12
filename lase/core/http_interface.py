f3a6ebf0351050553363e28c271a103c9545b422
minjung <minjung.kim@koheron.com>
2016-02-12 04:02:01 +0100

    Fix print http_interface.py

--- lase/core/http_interface.py
+++ lase/core/http_interface.py
@@ -5,11 +5,11 @@ import requests
 class HTTPInterface:
     def __init__(self, IP, port=80):
         self.url = 'http://' + IP + ':' + str(port)
-        
+
     def get_bistream_id(self):
         r = requests.get(self.url + '/bitstream_id')
         return r.text
-        
+
     def ping(self):
         r = requests.post(self.url + '/ping', data={})
         
@@ -22,7 +22,7 @@ class HTTPInterface:
         """
         zip_filename = name + '-' + version + '.zip'
         r = requests.post(self.url + '/deploy/remote/' + zip_filename, data={})
-        
+
     def deploy_local_instrument(self, name, version):
         zip_filename = name + '-' + version + '.zip'
         print('Deploying ' + zip_filename)
@@ -31,7 +31,7 @@ class HTTPInterface:
         except:
             pass
             #print('Timeout occured')
-        
+
     def remove_local_instrument(self, name, version):
         zip_filename = name + '-' + version + '.zip'
         r = requests.get(self.url + '/remove/local/' + zip_filename)
@@ -51,10 +51,10 @@ class HTTPInterface:
 
 if __name__ == "__main__":
     http = HTTPInterface('192.168.1.15')
-    print http.get_bistream_id()
+    print(http.get_bistream_id())
 #    http.ping()
 #    http.deploy_remote_instrument('spectrum', '06ee48f')
 #    http.deploy_local_instrument('oscillo', '06ee48f')
-#    print http.remove_local_instrument('oscillo', '06ee48f')
-    print http.get_local_instruments()
+#    print(http.remove_local_instrument('oscillo', '06ee48f'))
+    print(http.get_local_instruments())
     http.install_instrument("spectrum")
