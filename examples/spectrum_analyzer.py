7d9856a56c5edf3014651982e1d0557390da4575
jeanminet <jean.minet@gmail.com>
2016-03-02 16:18:24 +0100

    save the last spectrum

--- examples/spectrum_analyzer.py
+++ examples/spectrum_analyzer.py
@@ -31,12 +31,14 @@ driver.set_laser_current(current)
 # Plot parameters
 fig = plt.figure()
 ax = fig.add_subplot(111)
-x = np.fft.fftshift(driver.sampling.f_fft)
+x = 1e-6 * np.fft.fftshift(driver.sampling.f_fft)
 y = 10*np.log10(np.fft.fftshift(driver.spectrum))
 li, = ax.plot(x, y)
 fig.canvas.draw()
 ax.set_xlim((x[0],x[-1]))
-ax.set_ylim((100,300))
+ax.set_ylim((100,250))
+ax.set_xlabel('Frequency (MHz)')
+ax.set_ylabel('Power spectral density (dB)')
 
 while True:
     try:
@@ -45,6 +47,8 @@ while True:
         fig.canvas.draw()
         plt.pause(0.001)
     except KeyboardInterrupt:
+        # Save last spectrum in a csv file
+        np.savetxt("psd.csv", driver.spectrum, delimiter=",")
         driver.stop_laser()
         driver.close()
         break
