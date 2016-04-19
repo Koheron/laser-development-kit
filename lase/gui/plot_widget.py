d0f5e68adb88156070695c65c32b5a03cac279b4
thomas <th.vanderbruggen@gmail.com>
2016-04-19 14:37:14 +0200

    Remove timestamp

--- lase/gui/plot_widget.py
+++ lase/gui/plot_widget.py
@@ -22,32 +22,32 @@ class TimeRollingPlot(pg.PlotWidget):
     def __init__(self, n_pts=30000, *args, **kwargs):
         super(TimeRollingPlot, self).__init__(
                       *args,
-                      axisItems={'bottom': TimeAxisItem(orientation='bottom')},
+                      # axisItems={'bottom': TimeAxisItem(orientation='bottom')},
                       **kwargs)
 
         self.dataItem = pg.PlotDataItem(pen=(0, 4), clear=True, _callSync='off')
         self.addItem(self.dataItem)
 
         self.values = np.zeros(n_pts)
-        self.times = np.zeros(n_pts) + time.time()
-        self.idx = 0
+        # self.times = np.zeros(n_pts) + time.time()
 
     def set_axis(self):
         self.getPlotItem().setMouseEnabled(x=False, y=False)
         self.getPlotItem().enableAutoRange()
 
     def update(self, value):
-        x_index = (self.idx + np.arange(value.size)) % self.values.size
 
         self.values = np.roll(self.values, -value.size)
-        self.times = np.roll(self.times, -value.size)
-        self.times[-1] = time.time()
+        # self.times = np.roll(self.times, -value.size)
+        # self.times[-1] = time.time()
         for i in range(value.size):
             self.values[-1-i] = value[i]
-            self.times[-value.size+i] = self.times[-value.size-1] + i / (self.times[-1] - self.times[-value.size]) / value.size
+        #     self.times[-value.size+i] = self.times[-value.size-1] + i / (self.times[-1] - self.times[-value.size]) / value.size
         
-        self.dataItem.setData(self.times, self.values, clear=True)
-        self.getViewBox().setXRange(self.times[0], self.times[-1])
+        # self.dataItem.setData(self.times, self.values, clear=True)
+        # self.getViewBox().setXRange(self.times[0], self.times[-1])
+
+        self.dataItem.setData(np.arange(self.values.size), self.values, clear=True)
 
 # https://gist.github.com/friendzis/4e98ebe2cf29c0c2c232
 class TimeAxisItem(pg.AxisItem):
