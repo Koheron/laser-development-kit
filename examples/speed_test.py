d21648ab963e5ebdf2729e528fc31c8a9ce93772
jeanminet <jean.minet@gmail.com>
2016-05-25 20:58:36 +0200

    update speed_test

--- examples/speed_test.py
+++ examples/speed_test.py
@@ -10,17 +10,22 @@ import matplotlib.pyplot as plt
 from utilities import load_instrument
 from ldk.drivers import Oscillo
 
+cmd = os.getenv('CMD','get_adc')
+
 def speed_test(host, n_pts=1000):
     time_array = np.zeros(n_pts)
     client = load_instrument(host, instrument='oscillo')
     driver = Oscillo(client)
-    driver.set_averaging(True)
+    driver.set_averaging(False)
 
     t0 = time.time()
     t_prev = t0
 
     for i in range(n_pts):
-        driver.get_adc()
+        if cmd == 'get_adc':
+            driver.get_adc()
+        elif cmd == 'get_num_average':
+            driver.get_num_average()
         t = time.time()
         time_array[i] = t - t_prev
         print host, i, time_array[i]
