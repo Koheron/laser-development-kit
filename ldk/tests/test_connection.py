feaab7a2c2dfd0466cd498fd9771362f7180a8d4
Jean Minet <jean.minet@gmail.com>
2016-08-14 01:09:33 +0200

    Update test_connection.py

--- ldk/tests/test_connection.py
+++ ldk/tests/test_connection.py
@@ -3,7 +3,7 @@
 import os
 import pytest
 from koheron_tcp_client import KClient
-from ..utilities import HTTPInterface
+from ..utilities import install_instrument
 from ..drivers import Oscillo
 
 @pytest.mark.real
@@ -12,8 +12,7 @@ class TestConnection:
     def test(self):
         current_path = os.getcwd()
         host = os.getenv('HOST','192.168.1.100')
-        http = HTTPInterface(host)
-        http.install_instrument('oscillo')
+        install_instrument(host,'oscillo')
         client = KClient(host)  
         assert client.is_connected
         driver = Oscillo(client)
